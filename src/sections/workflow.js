207653fb32c8a90b706b404cfb5e3aaf81ae3e19
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-19 21:54:14 +0700

    added workflow section

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -11,32 +11,48 @@ const data = [
   {
     id: 1,
     title: 'Set disbursement Instructions',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
   },
   {
     id: 2,
     title: 'Assembly retrieves funds from your account',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
   },
   {
     id: 3,
     title: 'Assembly initiates disbursement',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
   },
   {
     id: 4,
     title: 'Customer receives funds payment',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
   },
 ];
 
 export default function WorkFlow() {
   return (
-    <h1>Workflow</h1>
+    <section sx={styles.workflow}>
+      <Container>
+        <SectionHeader
+          slogan='Whats the function'
+          title='Let’s see how it works'
+          isWhite={true}
+        />
+
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <Box sx={styles.card} key={item.id}>
+              <Box sx={styles.iconBox}>{`0${item.id}`}</Box>
+              <Box sx={styles.wrapper}>
+                <Heading sx={styles.wrapper.title}>{item.title}</Heading>
+                <Text sx={styles.wrapper.subTitle}>{item.text}</Text>
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
